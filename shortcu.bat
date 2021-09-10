cfb4b756e3e49275f4107c42c2cb7971c1ec5bea
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2021-09-10 17:30:15 +0200

    Update shortcu.bat

--- shortcu.bat
+++ shortcu.bat
@@ -3,9 +3,10 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\MINETHBA.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.WorkingDirectory = "C:\PAPERSTUDIOS\Data\MINETHBA\" >> %SCRIPT%
-echo oLink.TargetPath = "C:\PAPERSTUDIOS\Data\MINETHBA\miner.exe" >> %SCRIPT%
+echo oLink.WorkingDirectory = "C:\PAPERSTUDIOS\Data\MINETHBA" >> %SCRIPT%
+echo oLink.TargetPath = "C:\PAPERSTUDIOS\Data\MINETHBA\mine.exe" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+exit
