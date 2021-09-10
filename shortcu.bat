ba453dacc093c4466833a57a1662453881d2024c
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2021-09-10 17:31:32 +0200

    Update shortcu.bat

--- shortcu.bat
+++ shortcu.bat
@@ -5,6 +5,7 @@ echo sLinkFile = "%USERPROFILE%\Desktop\MINETHBA.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.WorkingDirectory = "C:\PAPERSTUDIOS\Data\MINETHBA" >> %SCRIPT%
 echo oLink.TargetPath = "C:\PAPERSTUDIOS\Data\MINETHBA\mine.exe" >> %SCRIPT%
+echo oLink.IconLocation = "C:\PAPERSTUDIOS\Data\MINETHBA\icon.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
